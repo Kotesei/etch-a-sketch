3439f9803c1494c3efb4bbda2108c4af4ca0ce4d
Kotesei <192398548+Kotesei@users.noreply.github.com>
2024-12-27 23:20:38 -0500

    Finished base of the project.

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 'use strict'
 const container = document.querySelector(".container")
+const btn = document.querySelector(".userInput")
 
 
 // Gets the grid-size
 function gridSize(size) {
+    if (size >= 51) {
+        alert("This is too much for the browser to handle! Max is 50.")
+        return
+    }
     let gridSize;
     let squareSize;
 gridSize = size * size
@@ -23,5 +28,20 @@ for (let square = 0; square < gridSize; square++) {
     })
 }
 
+btn.addEventListener("click", function() {
+   const input = window.prompt("Please type in how big you would like the grid!", "16")
+   let values = "0123456789"
+   let allow;
 
-gridSize(20)
+   input.split("").forEach((i) => {
+    if(values.includes(i)) {
+        allow = true;
+    } else {
+        allow = false;
+    }
+})
+   if (allow) {
+       container.innerHTML = ""
+       gridSize(input)
+   } else window.alert("Not a number!!!")
+})
