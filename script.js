a77eb2c4e4c6dde77dda615f69a5f428886aaeee
Kotesei <192398548+Kotesei@users.noreply.github.com>
2024-12-27 22:30:04 -0500

    Added grid and function to take in grid size

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict'
+const container = document.querySelector(".container")
+
+
+// Gets the grid-size
+function gridSize(size) {
+    let gridSize;
+    let squareSize;
+gridSize = size * size
+squareSize = (800 / gridSize) * size
+
+for (let square = 0; square < gridSize; square++) {
+    container.innerHTML += `<div class="square"></div>`
+    }
+
+    document.querySelectorAll(".square").forEach((el) => {
+        el.style.width = squareSize + "px"
+        el.style.height = squareSize + "px"
+
+        el.addEventListener("mouseenter", function() {
+            el.style.backgroundColor = "blue"
+        })
+    })
+}
+
+
+gridSize(20)
